27af7433960c7e20b92ad8b1147bb154d316c49a
aleushea <aleushea@gmail.com>
2024-05-09 05:21:35 +0300

    0x02-python_async_comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-'''
-Asynchronous Python
-'''
+''' Asynchronous Python '''
 import random
 import asyncio
 from typing import AsyncGenerator
 
-
-async def async_generator() -> AsyncGenerator[float, None, None]:
-    '''
-    yeild a random number at 1sec interval
-    '''
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''yield a random number at 1sec interval'''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
